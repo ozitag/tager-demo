30559a5b64fad2415eedfbf2fc45370c3cebd587
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-06 21:45:33 +0300

    update tager-demo

---
+++ scripts/deploy-develop.sh
@@ -0,0 +1,16 @@
+cd /srv/tager
+
+sudo chown www-data:www-data -R .
+
+sudo -u www-data git fetch --all
+sudo -u www-data git reset --hard origin/develop
+sudo -u www-data git clean -f -d
+
+sudo cp .env.development .env
+sudo cp web/.env.development web/.env
+sudo cp backend/.env.development backend/.env
+sudo cp admin/.env.development admin/.env
+
+sudo cp scripts/nginx-develop.conf /etc/nginx/sites-available/tager
+
+sudo docker-compose -f docker-compose.dev.yml --project-name tager up --build --force-recreate -d
